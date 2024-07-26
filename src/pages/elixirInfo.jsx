ae17aad4f8b38c625ce34e7a944e58480d9b3f0e
Kareena Patel <kareenapat17@gmail.com>
2024-07-26 11:38:41 +0100

    Added Await, defer and suspense - problem with obtaining data

--- src/pages/elixirInfo.jsx
+++ src/pages/elixirInfo.jsx
@@ -1,49 +1,29 @@
-import React from "react";
-import { useParams, Link, useLocation } from "react-router-dom";
+import React, { Suspense } from "react";
+import { useParams, Link, useLocation, defer, Await, useLoaderData } from "react-router-dom";
 import { v4 as uuidv4 } from 'uuid'; /* Generating unique id*/
+
+import { getAllElixirs } from "../ApiEndpoints/getAllElixirs";
 
 import isTonicOrElixir from "../reusableFunctions/isTonicOrElixir"
 import hylianLogo from "../assets/Hylian_Symbol.png"
 
+export function loader() {
+    return defer({ elixirDetail: getAllElixirs() })
+}
+
 export default function ElixirInfo() {
     const params = useParams()
-        // const location = useLocation() /*Where user is in application */
-    const [elixirDetail, setElixirDetail] = React.useState([]);
-    const [loading, setLoading] = React.useState(false)
+    // const location = useLocation() /*Where user is in application */
+    const dataPromise = useLoaderData()
+    console.log(dataPromise.elixirDetail)
 
-    React.useEffect(() => {
-        async function getElixirDetail() {
-            setLoading(true)
-            try {
-                const res = await fetch(`/api/${params.elixirName}`)
-                const data = await res.json()
-                setElixirDetail(data.items[0])
-            }
-            catch (err) {
-                console.log("Unable to load...")
-            }
-            setLoading(false)
-        }
-        getElixirDetail()
-    }, [params.elixirName])
+    function renderElement(elixirDetail) {
+        const ingredients = elixirDetail.ingredients
 
-    if (loading) {
         return (
-            <main id="loading__page" className="container col">
-                <img className="loadingLogo" src={hylianLogo} alt="" />
-                <h2>Loading...</h2>
-            </main>
-        )
-    }
-
-    const ingredients = elixirDetail.ingredients
-
-    return (
-        <main className="container">
-            <Link className="elixir__backlink" to="..">&#8678; Back to Elixirs and Tonics</Link>
             <div id="elixir__card-grid" className="elixir__card-info card grid">
                 <img id="elixir__card-img" src={`src/assets/Elixirs/${elixirDetail.imageURL}`} alt={`${isTonicOrElixir(elixirDetail.name)} image preview`} />
-                
+
                 <div id="elixir__card-head">
                     <h1 id="elixir__card-name">{isTonicOrElixir(elixirDetail.name)}</h1>
                     <p>Effect: {elixirDetail.effect}</p>
@@ -66,6 +46,26 @@ export default function ElixirInfo() {
 
                 </div>
             </div>
+        )
+    }
+    
+    function Loading(){
+        return (
+                    <main id="loading__page" className="container col">
+                        <img className="loadingLogo" src={hylianLogo} alt="" />
+                        <h2>Loading...</h2>
+                    </main>
+                )
+    }
+
+    return (
+        <main className="container">
+            <Link className="elixir__backlink" to="..">&#8678; Back to Elixirs and Tonics</Link>
+            <Suspense fallback={<Loading/>}>
+                <Await resolve={dataPromise.elixirDetail}>
+                    {renderElement}
+                </Await>
+            </Suspense>
         </main>
     );
 }
