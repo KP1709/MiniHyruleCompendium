7a982b3263a38f014536083612714818b8033b91
Kareena Patel <kareenapat17@gmail.com>
2024-07-26 10:52:51 +0100

    Changed API endpoint to account for name in url

--- src/ApiEndpoints/getAllElixirs.js
+++ src/ApiEndpoints/getAllElixirs.js
@@ -1,5 +1,6 @@
-export async function getAllElixirs() {
-    const res = await fetch("/api/")
+export async function getAllElixirs(name) {
+    const url = name ? `/api/${name}` : "/api/"
+    const res = await fetch(url)
     if (!res.ok){
         throw {
             message: "Failed to get elixirs and tonics", 
